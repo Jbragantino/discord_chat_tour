0ea78310a770ee20cef060f493e3a6eaecf93f49
Jbragantino <bragantinojoao@gmail.com>
2023-10-16 18:27:07 -0300

    Rasa 😋🦐🦼📁🐘

--- src/bot/client.py
+++ src/bot/client.py
@@ -1,27 +1,46 @@
 import discord
 from discord.ext import commands
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
 
-def create_bot() -> commands.Bot:
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        print(message.content)
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+def create_bot() -> discord.Client:
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    client = MyClient(intents=intents)
 
-    @bot.event
-    async def on_ready():
-        print(f"{bot.user.name} has connected to Discord!")
+    return client
+    
 
-    @bot.command(name="history", help="Return a list of historical facts about a city")
-    async def get_history(ctx: commands.Context, *city_name):
-        city = " ".join(city_name)
-        await ctx.send(f"It's history time about {city}")
+    # bot = commands.Bot(command_prefix="!", intents=intents)
 
-    @bot.command(
-        name="restaurants", help="Returns a list of interesting restaurants in a city"
-    )
-    async def get_restaurants(ctx: commands.Context, *city_name):
-        city = " ".join(city_name)
-        await ctx.send(f"Here are some restaurants in {city}")
+    # @bot.event
+    # async def on_ready():
+    #     print(f"{bot.user.name} has connected to Discord!")
 
-    return bot
+    # @bot.command(name="history", help="Return a list of historical facts about a city")
+    # async def get_history(ctx: commands.Context, *city_name):
+    #     city = " ".join(city_name)
+    #     await ctx.send(f"It's history time about {city}")
+
+    # @bot.command(
+    #     name="restaurants", help="Returns a list of interesting restaurants in a city"
+    # )
+    # async def get_restaurants(ctx: commands.Context, *city_name):
+    #     city = " ".join(city_name)
+    #     await ctx.send(f"Here are some restaurants in {city}")
+
+    # return bot
---
+++ src/rasa_config.py
@@ -0,0 +1,23 @@
+import os
+import rasa
+import nest_asyncio
+from rasa.cli.scaffold import create_initial_project
+from rasa.jupyter import chat
+
+nest_asyncio.apply()
+
+project = 'rasa_3x'
+create_initial_project(project)
+
+os.chdir(project)
+
+config = "config.yml"
+training_files = "data/"
+domain = "domain.yml"
+output = "models/"
+
+model_path = rasa.train(domain, config, [training_files], output)
+
+
+endpoints = None
+chat(model_path.model, endpoints)
